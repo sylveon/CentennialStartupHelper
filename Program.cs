7b35812df7af18b5ae61dd86c16c99b7c2f06aa6
Charles Milette <charles.milette@gmail.com>
2017-04-19 17:54:57 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Windows.ApplicationModel;
+
+namespace CentennialStartupHelper
+{
+    internal class Program
+    {
+        private static int Main(string[] CommandLine)
+        {
+            try
+            {
+                string Argument = "/status";
+                if (CommandLine.Length >= 1)
+                {
+                    Argument = CommandLine[0];
+                }
+
+                int startupTaskIndex = 0;
+                if (CommandLine.Length >= 2)
+                {
+                    startupTaskIndex = int.Parse(CommandLine[1]);
+                }
+
+                var task = StartupTask.GetForCurrentPackageAsync().AsTask().Result[startupTaskIndex];
+
+                switch (Argument)
+                {
+                    case "/enable":
+                        return task.RequestEnableAsync().AsTask().Result.ToInt();
+
+                    case "/disable":
+                        task.Disable();
+                        return task.State.ToInt();
+
+                    case "/status":
+                        return task.State.ToInt();
+
+                    default: throw new ArgumentException();
+                }
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+
+        private static int ToInt(StartupTaskState state)
+        {
+            switch (state)
+            {
+                case StartupTaskState.Enabled:
+                    return 0;
+
+                case StartupTaskState.Disabled:
+                    return 1;
+
+                case StartupTaskState.DisabledByUser:
+                    return 2;
+
+                default: throw new NotImplementedException();
+            }
+        }
+    }
+}
